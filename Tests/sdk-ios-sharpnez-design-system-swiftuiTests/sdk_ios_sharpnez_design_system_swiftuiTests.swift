1425269d2d92cb394a749a1b4036d5f3ef454f2f
Tiago Linhares <tiagolinharessouza@gmail.com>
2025-07-08 20:27:53 -0300

    Remove packages

--- Tests/sdk-ios-sharpnez-design-system-swiftuiTests/sdk_ios_sharpnez_design_system_swiftuiTests.swift
+++
@@ -1,6 +0,0 @@
-import Testing
-@testable import sdk_ios_sharpnez_design_system_swiftui
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
